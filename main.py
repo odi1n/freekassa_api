1a0f21e48628ea7baba3c8343fa15ae105e8b3f7
odi1n <sashf1999@mail.ru>
2022-02-15 22:57:03 +0300

    Реализация api FreeKassa

---
+++ main.py
@@ -0,0 +1,14 @@
+from freekassa_api import Merchant
+
+
+def test():
+    merchant = Merchant(shop_id=123456789,
+                        secret1="secret1",
+                        secret2="secret2",
+                        api_key="api_key")
+    # return merchant.get_payment_form_url(amount=123, order_id="dasd", us_={'token':'dasd',"test":"test"})
+    return merchant.get_balance()
+
+
+if __name__ == '__main__':
+    print(test())
